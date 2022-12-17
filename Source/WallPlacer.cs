7f60b1ec70a19a5de5eb54096afbdceb939ca239
David Mannes <git@davidmannes.de>
2022-12-17 18:03:25 +0100

    Add menu for wall placer

--- Source/WallPlacer.cs
+++ Source/WallPlacer.cs
@@ -3,29 +3,43 @@ using Godot;
 
 namespace BaseBuilding;
 
-public partial class WallPlacer : Node2D
+public partial class WallPlacer : Control
 {
 	[Export(PropertyHint.ResourceType, nameof(Texture2D))]
 	private Texture2D _texture = null!;
 
+	public override void _Ready()
+	{
+		SetEnabled(false);
+	}
+
+	public void SetEnabled(bool enabled)
+	{
+		SetProcessUnhandledInput(enabled);
+	}
+
 	public override void _UnhandledInput(InputEvent @event)
 	{
 		Debug.Assert(_texture != null);
-		  
+
 		if (@event.IsActionPressed(InputActions.MouseclickLeft))
 		{
 			var tileMap = GetNode<TileMap>("%Map");
-			var mousePosition = GetLocalMousePosition();	
+			var mousePosition = tileMap.GetGlobalMousePosition();	
 			var mapPosition = tileMap.MapToLocal(tileMap.LocalToMap(mousePosition));
 			
 			tileMap.AddChild(new Sprite2D
 			{
 				Texture = _texture,
 				GlobalPosition = mapPosition,
-				ZIndex = 1
+				ZIndex = 1,
 			});
-			
+
 			GetTree().Root.SetInputAsHandled();
+		}
+		else if (@event.IsActionPressed(InputActions.MouseclickRight))
+		{
+			SetEnabled(false);
 		}
 	}
 }
